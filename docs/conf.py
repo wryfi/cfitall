887082767f264c4949c07cf86ca25e7343ac56e7
Chris Haumesser <5400416-wryfi@users.noreply.gitlab.com>
2022-04-11 16:32:10 -0700

    try to fix docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 
+import sphinx_rtd_theme
+
 here = os.path.dirname(os.path.abspath(__file__))
 cfitall_path = os.path.join(here, "..", "cfitall")
 sys.path.insert(0, cfitall_path)
@@ -71,11 +73,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# Readthedocs theme
-# on_rtd is whether on readthedocs.org, this line of code grabbed from docs.readthedocs.org...
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
